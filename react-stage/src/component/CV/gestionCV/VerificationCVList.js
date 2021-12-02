1d0a48f3ae5bf402a9c417861abfa6d82b274b70
Neil Carrie <neil.carrie@gmail.com>
2021-12-02 11:26:19 -0500

    Fix css

--- react-stage/src/component/CV/gestionCV/VerificationCVList.js
+++ react-stage/src/component/CV/gestionCV/VerificationCVList.js
@@ -23,7 +23,7 @@ const VerificationCVList = ({ cvList, onClickCV }) => {
   return (
     <div className="container">
       <h1 className="text-white">Liste des CV des étudiants</h1>
-      <Table striped bordered hover variant="cvDark">
+      <Table striped bordered hover variant="dark">
         <thead>
           <tr>
             <th>Nom d'étudiant</th>
